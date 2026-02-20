04c242bb0992a047f8b9399cf26cb308ce29ed37
Claudia Nguyen <158665328+toro-claudia@users.noreply.github.com>
2026-02-19 19:08:23 -0600

    Initialize project

---
+++ backend/src/main/java/CreatorXpApplication.java
@@ -0,0 +1,13 @@
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorXpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreatorXpApplication.class, args);
+	}
+
+}
